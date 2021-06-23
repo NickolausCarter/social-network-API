8460296f757969018c4c68c960d2eff2d52f49d2
Pure KaoZ <nickolaus.carter81@gmail.com>
2021-06-22 21:40:57 -0500

    created User model

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: [/.+@.+\..+/, 'Invalid e-mail address']
+    },
+    thoughts: [{ type: Schema.Types.ObjectId, ref: 'Thought' }],
+    friends: [{ type: Schema.Types.ObjectId, ref: 'User' }]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+);
+
+// returns number of total friends for user
+userSchema.virtual('friendCount').get(function() {
+  return this.friends.length;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
