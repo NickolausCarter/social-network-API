1e8abb503dc0ee52119f6d3792c3a4f68d86ff93
Pure KaoZ <nickolaus.carter81@gmail.com>
2021-06-24 15:19:02 -0500

    user controller and routes built

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -35,8 +35,68 @@ const userController = {
         .catch(err => res.status(400).json(err));
   },
 
+  // PUT to update user api/users/:id
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true
+    })
+    .then(userData => {
+      if (!userData) {
+        res.status(404).json({ message: 'No user found with this ID!' });
+        return;
+      }
+      res.json(userData);
+    })
+    .catch(err => res.status(400).json(err));
+  },
 
+  // DELETE user api/users/:id
+  deleteUser({ params }, res) {
+    User.findOneAndDelete({ _id: params.id })
+        .then(userData => {
+          if (!userData) {
+            res.status(404).json({ message: 'No user found with this ID!' });
+            return;
+          }
+          res.json(userData);
+        })
+        .catch(err => res.status(400).json(err));
+  },
 
-}
+  // POST add friend to user api/users/:userId/friends/:friendId
+  addFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $push: { friends: params.friendId } },
+      { new: true, rundValidators: true }
+    )
+    .then(userData => {
+      if (!userData) {
+        res.status(404).json({ message: 'No user found with this ID!' });
+        return;
+      }
+      res.json(userData);
+    })
+    .catch(err => res.status(400).json(err));
+  },
+
+  // DELETE friend from user api/users/:userId/friends/:friendId
+  deleteFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $pull: { friends: params.friendId } },
+      { new: true }
+    )
+    .then(userData => {
+      if (!userData) {
+        res.status(404).json({ message: 'No user found with this ID!' });
+        return;
+      }
+      res.json(userData);
+    })
+    .catch(err => res.status(400).json(err));
+  }
+};
 
 module.exports = userController;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -3,7 +3,11 @@ const router = require('express').Router();
 const {
   getAllUsers,
   getUserById,
-  createUser
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  deleteFriend
 } = require('../../controllers/user-controller');
 
 router.route('/')
@@ -12,5 +16,11 @@ router.route('/')
 
 router.route('/:id')
       .get(getUserById)
+      .put(updateUser)
+      .delete(deleteUser)
+
+router.route('/:userId/friends/:friendId')
+      .post(addFriend)
+      .delete(deleteFriend)
 
 module.exports = router;
