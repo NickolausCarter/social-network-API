1b4b573649e127400a90c7d17a50dd6c8d09761b
Pure KaoZ <nickolaus.carter81@gmail.com>
2021-06-24 17:10:43 -0500

    modified thought-controller and enabled route in index.js

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -39,13 +39,13 @@ const thoughtController = {
             { $push: { thoughts: thoughtData._id } },
             { new: true }
           )
-        })
-        .then(userData => {
-          if (!userData) {
-            res.status(404).json({ message: 'No user found with that ID!' });
-            return;
-          }
-          res.json(userData);
+          .then(userData => {
+            if (!userData) {
+              res.status(404).json({ message: 'No user found with that ID!' });
+              return;
+            }
+            res.json(userData);
+          })
         })
         .catch(err => res.status(400).json(err));
   },
